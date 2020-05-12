b370d03b0936d56f603c894fb0c3f5f012e6add3
Your Name <you@example.com>
2020-05-12 18:26:54 +0500

    fixed

--- first_project/first_app/forms.py
+++ first_project/first_app/forms.py
@@ -14,8 +14,8 @@ class TestForm(forms.Form):
 
     date_field= forms.DateField(initial="2020-20-5",widget=forms.SelectDateWidget(years=YEARS))
     text = forms.CharField(label="My text",min_length=7,widget=forms.Textarea) 
-    radio_choices = forms.CharField(min_length=7,widget=forms.RadioSelect(choices=SOME_CHOICES)) 
-    checkbox_choices = forms.CharField(min_length=7,widget=forms.CheckboxSelectMultiple(choices=SOME_CHOICES)) 
+    radio_choices = forms.CharField(widget=forms.RadioSelect(choices=SOME_CHOICES)) 
+    checkbox_choices = forms.CharField(widget=forms.CheckboxSelectMultiple(choices=SOME_CHOICES)) 
     boolean = forms.BooleanField()
     integer = forms.IntegerField(initial=10,widget=forms.Select(choices=INTS_CHOICES))
     email = forms.EmailField()
